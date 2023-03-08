b8e6276a3442b6c26381c00025e51c4da1db4b41
Victor Fernandes <victor.santos136@fatec.sp.gov.br>
2023-03-08 19:08:52 -0300

    att

--- Strategy/math_strategy_bertoti/src/math/strategy/bertoti/Operation.java
+++ Strategy/math_strategy_bertoti/src/math/strategy/bertoti/Operation.java
@@ -1,29 +1,25 @@
 package math.strategy.bertoti;
 
 class SumOperation implements OperationStrategy {
-    @Override
     public int mathOperation(int A, int B) {
         return A + B;
     }
 }
 
 class SubOperation implements OperationStrategy {
-    @Override
     public int mathOperation(int A, int B) {
         return A - B;
     }
 }
 
 class MultiOperation implements OperationStrategy {
-    @Override
     public int mathOperation(int A, int B) {
         return A * B;
     }
 }
 
 class DivOperation implements OperationStrategy {
-    @Override
-    public int mathOperation(int A, int B) {
+	public int mathOperation(int A, int B) {
         return A / B;
     }
 }
